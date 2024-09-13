919a98c3679bc5e9e543afa167d8a8b061742ccd
Randall Winkhart <idgr@tutanota.com>
2024-09-12 21:11:53 -0400

    Use \033 in place of \x1b in newline_test.go (for consistency)

--- test/newline_test.go
+++ test/newline_test.go
@@ -16,17 +16,17 @@ func TestNewline(t *testing.T) {
 		{
 			name:     "Minimal spacing + <br> line break",
 			input:    []string{"- First item", "\t- Sub item 1", "\t- Sub item 2", "- Second item", "# Heading 1", "Paragraph text", "that spans two lines.", "", "New paragraph.", "# Header 1", "__*Fancy*__ paragraph<br>", "with a ~~line break~~."},
-			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\x1b[1m\x1b[4mHeading 1\x1b[0m\nParagraph text that spans two lines. \n\nNew paragraph. \n\n\x1b[1m\x1b[4mHeader 1\x1b[0m\n\x1b[1m\x1b[3mFancy\x1b[0m\x1b[0m paragraph\nwith a \x1b[9mline break\x1b[0m. ",
+			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\033[1m\033[4mHeading 1\033[0m\nParagraph text that spans two lines. \n\nNew paragraph. \n\n\033[1m\033[4mHeader 1\033[0m\n\033[1m\033[3mFancy\033[0m\033[0m paragraph\nwith a \033[9mline break\033[0m. ",
 		},
 		{
 			name:     "Standard spacing + double-space line break",
 			input:    []string{"- First item", "\t- Sub item 1", "\t- Sub item 2", "- Second item", "", "# Heading 1", "Paragraph text", "that spans two lines.", "", "New paragraph.", "", "# Header 1", "__*Fancy*__ paragraph  ", "with a ~~line break~~."},
-			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\x1b[1m\x1b[4mHeading 1\x1b[0m\nParagraph text that spans two lines. \n\nNew paragraph. \n\n\x1b[1m\x1b[4mHeader 1\x1b[0m\n\x1b[1m\x1b[3mFancy\x1b[0m\x1b[0m paragraph\nwith a \x1b[9mline break\x1b[0m. ",
+			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\033[1m\033[4mHeading 1\033[0m\nParagraph text that spans two lines. \n\nNew paragraph. \n\n\033[1m\033[4mHeader 1\033[0m\n\033[1m\033[3mFancy\033[0m\033[0m paragraph\nwith a \033[9mline break\033[0m. ",
 		},
 		{
 			name:     "Exaggerated spacing + double-space line break",
 			input:    []string{"- First item", "\t- Sub item 1", "\t- Sub item 2", "- Second item", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "# Heading 1", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "Paragraph text", "that spans two lines.", "", "New paragraph.", "", "", "", "", "", "", "", "# Header 1", "__*Fancy*__ paragraph  ", "with a ~~line break~~."},
-			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\x1b[1m\x1b[4mHeading 1\x1b[0m\nParagraph text that spans two lines. \n\nNew paragraph. \n\n\x1b[1m\x1b[4mHeader 1\x1b[0m\n\x1b[1m\x1b[3mFancy\x1b[0m\x1b[0m paragraph\nwith a \x1b[9mline break\x1b[0m. ",
+			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\033[1m\033[4mHeading 1\033[0m\nParagraph text that spans two lines. \n\nNew paragraph. \n\n\033[1m\033[4mHeader 1\033[0m\n\033[1m\033[3mFancy\033[0m\033[0m paragraph\nwith a \033[9mline break\033[0m. ",
 		},
 	}
 	for _, tt := range tests {
