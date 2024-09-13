673c83800cd3c396eed24a3eceea90effd6f4d6e
Randall Winkhart <idgr@tutanota.com>
2024-09-12 21:14:44 -0400

    Add test coverage for emphasized (new) paragraphs

--- test/newline_test.go
+++ test/newline_test.go
@@ -14,17 +14,17 @@ func TestNewline(t *testing.T) {
 	}{
 		// New line testing (relationship between headers/list/paragraphs)
 		{
-			name:     "Minimal spacing + <br> line break",
+			name:     "Minimal spacing + <br> line break + standard middle paragraph",
 			input:    []string{"- First item", "\t- Sub item 1", "\t- Sub item 2", "- Second item", "# Heading 1", "Paragraph text", "that spans two lines.", "", "New paragraph.", "# Header 1", "__*Fancy*__ paragraph<br>", "with a ~~line break~~."},
 			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\033[1m\033[4mHeading 1\033[0m\nParagraph text that spans two lines. \n\nNew paragraph. \n\n\033[1m\033[4mHeader 1\033[0m\n\033[1m\033[3mFancy\033[0m\033[0m paragraph\nwith a \033[9mline break\033[0m. ",
 		},
 		{
-			name:     "Standard spacing + double-space line break",
-			input:    []string{"- First item", "\t- Sub item 1", "\t- Sub item 2", "- Second item", "", "# Heading 1", "Paragraph text", "that spans two lines.", "", "New paragraph.", "", "# Header 1", "__*Fancy*__ paragraph  ", "with a ~~line break~~."},
-			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\033[1m\033[4mHeading 1\033[0m\nParagraph text that spans two lines. \n\nNew paragraph. \n\n\033[1m\033[4mHeader 1\033[0m\n\033[1m\033[3mFancy\033[0m\033[0m paragraph\nwith a \033[9mline break\033[0m. ",
+			name:     "Standard spacing + double-space line break + emphasized middle paragraph",
+			input:    []string{"- First item", "\t- Sub item 1", "\t- Sub item 2", "- Second item", "", "# Heading 1", "Paragraph text", "that spans two lines.", "", "*New* paragraph.", "", "# Header 1", "__*Fancy*__ paragraph  ", "with a ~~line break~~."},
+			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\033[1m\033[4mHeading 1\033[0m\nParagraph text that spans two lines. \n\n\033[3mNew\033[0m paragraph. \n\n\033[1m\033[4mHeader 1\033[0m\n\033[1m\033[3mFancy\033[0m\033[0m paragraph\nwith a \033[9mline break\033[0m. ",
 		},
 		{
-			name:     "Exaggerated spacing + double-space line break",
+			name:     "Exaggerated spacing + double-space line break + standard middle paragraph",
 			input:    []string{"- First item", "\t- Sub item 1", "\t- Sub item 2", "- Second item", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "# Heading 1", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "Paragraph text", "that spans two lines.", "", "New paragraph.", "", "", "", "", "", "", "", "# Header 1", "__*Fancy*__ paragraph  ", "with a ~~line break~~."},
 			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\033[1m\033[4mHeading 1\033[0m\nParagraph text that spans two lines. \n\nNew paragraph. \n\n\033[1m\033[4mHeader 1\033[0m\n\033[1m\033[3mFancy\033[0m\033[0m paragraph\nwith a \033[9mline break\033[0m. ",
 		},
