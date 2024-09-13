fee6766e1b8e4f315699a80189b12a97a57cb3d7
Randall Winkhart <idgr@tutanota.com>
2024-09-12 20:51:48 -0400

    Add test for newline relationships between headers/lists/paragraphs (currently failing due to paragraph spacing)

---
+++ test/newline_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"testing"
+
+	bean "github.com/Trojan2021/BEAN/render"
+)
+
+func TestNewline(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []string
+		expected string
+	}{
+		// New line testing (relationship between headers/list/paragraphs)
+		{
+			name:     "Minimal spacing + <br> line break",
+			input:    []string{"- First item", "\t- Sub item 1", "\t- Sub item 2", "- Second item", "# Heading 1", "Paragraph text", "that spans two lines.", "", "New paragraph.", "# Header 1", "__*Fancy*__ paragraph<br>", "with a ~~line break~~."},
+			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\x1b[1m\x1b[4mHeading 1\x1b[0m\nParagraph text that spans two lines. \n\nNew paragraph. \n\n\x1b[1m\x1b[4mHeader 1\x1b[0m\n\x1b[1m\x1b[3mFancy\x1b[0m\x1b[0m paragraph\nwith a \x1b[9mline break\x1b[0m. ",
+		},
+		{
+			name:     "Standard spacing + double-space line break",
+			input:    []string{"- First item", "\t- Sub item 1", "\t- Sub item 2", "- Second item", "", "# Heading 1", "Paragraph text", "that spans two lines.", "", "New paragraph.", "", "# Header 1", "__*Fancy*__ paragraph  ", "with a ~~line break~~."},
+			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\x1b[1m\x1b[4mHeading 1\x1b[0m\nParagraph text that spans two lines. \n\nNew paragraph. \n\n\x1b[1m\x1b[4mHeader 1\x1b[0m\n\x1b[1m\x1b[3mFancy\x1b[0m\x1b[0m paragraph\nwith a \x1b[9mline break\x1b[0m. ",
+		},
+		{
+			name:     "Exaggerated spacing + double-space line break",
+			input:    []string{"- First item", "\t- Sub item 1", "\t- Sub item 2", "- Second item", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "# Heading 1", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "Paragraph text", "that spans two lines.", "", "New paragraph.", "", "", "", "", "", "", "", "# Header 1", "__*Fancy*__ paragraph  ", "with a ~~line break~~."},
+			expected: "• First item\n    • Sub item 1\n    • Sub item 2\n• Second item\n\n\x1b[1m\x1b[4mHeading 1\x1b[0m\nParagraph text that spans two lines. \n\nNew paragraph. \n\n\x1b[1m\x1b[4mHeader 1\x1b[0m\n\x1b[1m\x1b[3mFancy\x1b[0m\x1b[0m paragraph\nwith a \x1b[9mline break\x1b[0m. ",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			output := bean.RenderMarkdown(tt.input)
+			if output != tt.expected {
+				bufferFailure(t, output, tt.expected)
+			}
+		})
+	}
+}
