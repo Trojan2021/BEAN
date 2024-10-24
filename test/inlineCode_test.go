616fe093c9830dbcdd9ebe7d97549668f1475a33
Randall Winkhart <idgr@tutanota.com>
2024-10-24 19:54:10 -0400

    Update inlineCode_test.go to account for word wrapping

--- test/inlineCode_test.go
+++ test/inlineCode_test.go
@@ -36,7 +36,7 @@ func TestInlineCode(t *testing.T) {
 		{
 			name:     "Used in a paragraph (multi-line; all forms of emphasis)",
 			input:    []string{"**Paragraph** _with_ `code` ~~in it~~ `**and such**`.", "Code blocks can be **_~~`emphasized`~~_** externally."},
-			expected: "\033[1mParagraph\033[0m \033[3mwith\033[0m \033[48;5;238;38;5;1mcode\033[0m \033[9min it\033[0m \033[48;5;238;38;5;1m**and such**\033[0m. Code blocks can be \033[1m\033[3m\033[9m\033[48;5;238;38;5;1memphasized\033[0m\033[0m\033[0m\033[0m externally.",
+			expected: "\033[1mParagraph\033[0m \033[3mwith\033[0m \033[48;5;238;38;5;1mcode\033[0m \033[9min it\033[0m \033[48;5;238;38;5;1m**and such**\033[0m. Code blocks can be \033[1m\033[3m\033[9m\033[48;5;238;38;5;1memphasized\033[0m\033[0m\033[0m\033[0m\nexternally.",
 		},
 		{
 			name:     "Used in a list",
